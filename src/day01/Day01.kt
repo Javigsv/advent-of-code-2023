aa130d736c9a595781ffa9e90f70dd7691a101f0
Javier-gsv <javier.garciasanvicente@bontouch.com>
2023-12-05 10:57:26 +0100

    Day 1

---
+++ src/day01/Day01.kt
@@ -0,0 +1,60 @@
+package day01
+
+import println
+import readInput
+
+
+val possibleValues = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+    "1" to "1",
+    "2" to "2",
+    "3" to "3",
+    "4" to "4",
+    "5" to "5",
+    "6" to "6",
+    "7" to "7",
+    "8" to "8",
+    "9" to "9"
+)
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { line ->
+            val first = line.first { character ->
+                character.isDigit()
+            }
+            val last = line.last { character ->
+                character.isDigit()
+            }
+            "$first$last".toInt()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.sumOf { line ->
+            val first = possibleValues
+                .mapNotNull { (textForm, value) ->
+                    (line.indexOf(textForm) to value).takeIf { (index, _) -> index >= 0 }
+                }.minBy { (index, _) -> index }.second
+            val last = possibleValues
+                .mapNotNull { (textForm, value) ->
+                    (line.lastIndexOf(textForm) to value).takeIf { (index, _) -> index >= 0 }
+                }.maxBy { (index, _) -> index }.second
+            "$first$last".toInt()
+        }
+    }
+
+    val input = readInput("day01", "Day01_input")
+
+    part1(input).println()
+    part2(input).println()
+}
